84fc4309a8a286a113633e074148bd068191002f
Marcelo <marcelovqvd@gmail.com>
2020-06-18 23:32:35 -0300

    Tests: 9 passed, 9 total

--- src/app.js
+++ src/app.js
@@ -34,16 +34,20 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
+  const repositoryLikes = repositories.find(repository => repository.id === id);
+  
   if(repositoryIndex < 0) {
     return response.status(400).json({ message: 'Repository not found'})
   }
-
+  
+  const likes = repositoryLikes.likes;
+  
   const repository = {
     id,
     title,
     url,
-    techs
+    techs,
+    likes,
   };
 
   repositories[repositoryIndex] = repository;
@@ -60,12 +64,31 @@ app.delete("/repositories/:id", (request, response) => {
     return response.status(400).json({ message: 'Repository not found'})
   }
 
-  repositories.splice(repositoryId);
+  repositories.splice(repositoryIndex);
   return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  
+  const { title, url, techs, likes } = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryLikes = repositories.find(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ message: 'Repository not found'})
+  } 
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositoryLikes.likes + 1
+  };
+
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);
 });
 
 module.exports = app;
